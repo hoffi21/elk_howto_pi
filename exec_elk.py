06e3256b1b1d14681cae4026ab71c566d2f68560
Mario <s122114@hft-leipzig.de>
2016-05-14 16:23:26 +0200

    add one more task

--- exec_elk.py
+++ exec_elk.py
@@ -13,22 +13,35 @@ host2 = '10.20.30.252'
 host_dec = input('Which host do you want to manage? ')
 if host_dec == 1:
     host = host1
+    user = raw_input('username: ')
+    password = getpass.getpass('password: ')
+
+    client = paramiko.SSHClient()
+    #client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #client.get_host_keys().add('suricata.local', 'ssh-ecdsa', key)
+    client.connect(host1, username=user, password=password)
+    stdin, stdout, stderr = client.exec_command('/usr/src/elasticsearch-2.2.1/bin/elasticsearch')
+    client.close()
+
+    user = raw_input('root username: ')
+    password = getpass.getpass('root password: ')
+
+    client = paramiko.SSHClient()
+    #client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host1, username=user, password=password)
+    stdin, stdout, stderr = client.exec_command('cd /etc/init.d/ && ./kibana_init')
+    client.close()
 elif host_dec == 2:
     host = host2
 else:
     print('Not a known host.')
 if host_dec != 1 and host_dec != 2:
+    print('Not a known host.')
     sys.exit()
-else:
-    user = raw_input('username: ')
-    password = getpass.getpass('password: ')
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #client.get_host_keys().add('suricata.local', 'ssh-ecdsa', key)
-    client.connect(host1, username=user, password=password)
-    stdin, stdout, stderr = client.exec_command('/usr/src/elasticsearch-2.2.1/bin/elasticsearch')
-client.close()
+
 
 
 #stdin, stdout, stderr = client.exec_command('/var/www/kibana-4.4.1-linux-x86/bin/kibana')
